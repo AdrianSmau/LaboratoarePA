127e3702153fb24865d299a77c0c20694c4e272c
AdrianSmau <62338436+AdrianSmau@users.noreply.github.com>
2021-03-17 23:51:13 +0200

    Delete PA_Lab5 directory

--- PA_Lab5/README.md
+++
@@ -1,3 +0,0 @@
-Partea obligatorie a fost implementata!
-
-Urmeaza a fi implementate si partile Optionala si Bonus!
--- PA_Lab5/src/com/exceptions/EmptyCatalogException.java
+++
@@ -1,9 +0,0 @@
-package com.exceptions;
-
-import com.catalog.Catalog;
-
-public class EmptyCatalogException extends RuntimeException {
-    public EmptyCatalogException(Catalog catalog) {
-        super("The catalog: " + catalog.getName() + "is empty!...");
-    }
-}
