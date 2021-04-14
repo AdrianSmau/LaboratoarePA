d810bae8e6523487771c2754eaee2ee0d02a4924
AdrianSmau <62338436+AdrianSmau@users.noreply.github.com>
2021-04-14 20:49:29 +0300

    Delete PA_Lab7 directory

--- PA_Lab7/src/CustomErrors/NotMyTurnException.java
+++
@@ -1,7 +0,0 @@
-package CustomErrors;
-
-public class NotMyTurnException extends Exception{
-    public NotMyTurnException(){
-        super("It's not my turn! I will let another thread take my place!...");
-    }
-}
